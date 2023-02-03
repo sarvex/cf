f0186d6295452781755fe6d4078f0f2c57b27777
Andrew Kelley <andrew@ziglang.org>
2023-02-03 15:42:05 -0700

    build.zig: expose a zig module

--- build.zig
+++ build.zig
@@ -4,6 +4,14 @@ pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const module = b.addModule(.{
+        .name = "cf",
+        .source_file = .{ .path = "cf.zig" },
+    });
+
+    // We do not have an example of using the zig module yet.
+    _ = module;
+
     const lib = b.addStaticLibrary(.{
         .name = "cf",
         .root_source_file = .{ .path = "cf.zig" },
