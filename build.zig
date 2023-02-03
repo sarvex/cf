7d7c7c80771f30c716b252343d87fc0139e7c9e4
Andrew Kelley <andrew@ziglang.org>
2023-02-03 15:31:04 -0700

    update build.zig to latest zig API

--- build.zig
+++ build.zig
@@ -1,16 +1,22 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary("cf", "cf.zig");
-    lib.setBuildMode(mode);
+    const lib = b.addStaticLibrary(.{
+        .name = "cf",
+        .root_source_file = .{ .path = "cf.zig" },
+        .optimize = optimize,
+        .target = target,
+    });
     lib.install();
 
-    const main_tests = b.addTest("cf.zig");
-    main_tests.setBuildMode(mode);
+    const main_tests = b.addTest(.{
+        .root_source_file = .{ .path = "cf.zig" },
+        .optimize = optimize,
+        .target = target,
+    });
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
